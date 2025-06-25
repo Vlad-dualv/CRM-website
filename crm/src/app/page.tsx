ce7081599064dc85c0ff1f1a184083d81c06172a
Vlad <vladswork95@gmail.com>
2025-06-25 10:55:00 +0100

    client and server components

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,13 +1,17 @@
-import { headers } from 'next/headers';
-
 import AddCompanyButton from './components/add-company-button';
+import ClientComponent from './components/client-component';
+import ServerComponent from './components/server-component';
+import ServerComponentCopy from './components/server-component-copy';
 
 export default function Home() {
-  console.log(headers());
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
+      <ServerComponent />
+      <ClientComponent>
+        <ServerComponentCopy />
+      </ClientComponent>
     </main>
   );
 }
