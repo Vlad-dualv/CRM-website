0e30821e8b53b99028bc1e04fe20f9978d36abd2
Vlad <vladswork95@gmail.com>
2025-06-27 10:05:36 +0100

    Added components

---
+++ crm/src/app/companies/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../components/header';
+
+export default function Page() {
+  return (
+    <div>
+      <Header>Companies</Header>
+    </div>
+  );
+}
